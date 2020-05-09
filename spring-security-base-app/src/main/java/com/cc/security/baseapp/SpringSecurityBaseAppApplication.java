a34c25cf16544342d672c247f192220f33568fcd
musibs <musib.somnath@gmail.com>
2020-05-09 18:05:30 +1000

    Created parent maven project and added the base application

---
+++ spring-security-base-app/src/main/java/com/cc/security/baseapp/SpringSecurityBaseAppApplication.java
@@ -0,0 +1,13 @@
+package com.cc.security.baseapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityBaseAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityBaseAppApplication.class, args);
+	}
+
+}
---
+++ spring-security-base-app/src/test/java/com/cc/security/baseapp/SpringSecurityBaseAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cc.security.baseapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityBaseAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
